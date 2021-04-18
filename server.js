49b7bde46db2d0dfbc8b7a1e219bffca706410df
Danilo Nascimento <danilo.nascimento.webdesign@gmail.com>
2021-04-18 16:03:40 -0300

    Adição de informações e o uso de arquivos estáticos

--- server.js
+++ server.js
@@ -3,11 +3,23 @@
 
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
+/**
+ * @link https://expressjs.com/pt-br/api.html#express.static
+ */
+
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, Wordl');
+  res.sendFile(path.join(__dirname, '/static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, '/static/speakers.html'));
 });
 
 app.listen(port, () => {
